79ccb8d742f0fe4f2d1a8e8a70897803decd2c7a
Dan Vera <dan@yovu.co>
2015-10-06 01:20:38 -0400

    lol

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'bundler'
+Bundler.require
+
+get '/' do
+  "Asere"
+end
+
+post '/mira' do
+  content_type :json
+
+  @body = ""
+  @image = ""
+
+  @resp = {
+    meta: {
+      code: 200
+    }
+  }
+
+  @resp.to_json
+end
