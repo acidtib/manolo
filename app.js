941c541014b0236af326c0d01e22850121c11282
Dan Vera <dan@yovu.co>
2015-10-06 14:16:25 -0400

    aver

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express')
-var app = express()
-
-app.set('port', (process.env.PORT || 5000))
+  , app = express()
+  , port = process.env.PORT || 3000
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
@@ -35,6 +34,6 @@ app.get('/mira', function(request, response) {
   response.json(response_payload);
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Listening on port:" + app.get('port'))
+app.listen(port, function() {
+  console.log('Listening on port ' + port)
 })
