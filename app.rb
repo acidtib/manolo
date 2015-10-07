1a28bb0cd0bbb46f410da3b625c8f663ed7627ef
Dan Vera <dan@yovu.co>
2015-10-07 03:06:13 -0400

    DALE

--- app.rb
+++ app.rb
@@ -8,16 +8,12 @@ end
 post '/mira' do
   content_type :json
 
-  logger.info "#{params}"
-
   @body = params['Body']
 
   @image = HTTParty.get(params['MediaUrl0'], follow_redirects: true)
   @image = @image.request.last_uri.to_s
 
   @upload_image = Cloudinary::Uploader.upload(@image)
-
-  logger.info @upload_image
 
   @bot_payload = {
     text: @body,
@@ -26,18 +22,18 @@ post '/mira' do
     attachments: {
       fallback: "deja el drama",
       color: "#D00000",
-      image_url: @image
+      image_url: @upload_image.url
     }
   }
 
   logger.info @bot_payload
 
-  # @to_slack = HTTParty.post(ENV['SLACK_HOOK'], { 
-  #   :body => @bot_payload.to_json,
-  #   :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-  # })
+  @to_slack = HTTParty.post(ENV['SLACK_HOOK'], { 
+    :body => @bot_payload.to_json,
+    :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+  })
 
-  # logger.info @to_slack
+  logger.info @to_slack
 
   @resp = {
     meta: {
